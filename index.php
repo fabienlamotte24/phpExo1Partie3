e6913786620b979d72a6e41a1489b8fca6f8dd88
fabienlamotte24 <fabien.lamotte00@outlook.fr>
2018-07-26 13:36:06 +0200

    exo1p3 php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercice 1</title>
+  </head>
+  <body>
+    <p>
+<?php
+  $incr = 0;
+  while($incr <= 10){
+    echo $incr . ' ';
+    $incr ++;
+  }
+ ?>
+    </p>
+  </body>
+</html>
